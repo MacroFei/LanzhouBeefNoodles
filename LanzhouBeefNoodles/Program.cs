95cb8a51545fe8251dfe88e3d57f82caad17631a
CCCMacro <44434429+MacroFei@users.noreply.github.com>
2020-01-07 14:56:20 +0800

    优化

--- LanzhouBeefNoodles/Program.cs
+++ LanzhouBeefNoodles/Program.cs
@@ -16,22 +16,22 @@ namespace LanzhouBeefNoodles
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
-            //var host = CreateWebHostBuilder(args).Build();
-            //using (var scope = host.Services.CreateScope())
-            //{
-            //    var services = scope.ServiceProvider;
-            //    try
-            //    {
-            //        var context = services.GetRequiredService<AppDbContext>();
-            //        DbInitializer.Seed(context);
-            //    }
-            //    catch (Exception)
-            //    {
+            //CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<AppDbContext>();
+                    DbInitializer.Seed(context);
+                }
+                catch (Exception)
+                {
 
-            //    }
-            //}
-            //host.Run();
+                }
+            }
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
